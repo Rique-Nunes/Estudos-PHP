fb4e93b549b508f86ed1ef998967e99cc1274c36
Henrique <91799317+Rique-Nunes@users.noreply.github.com>
2025-08-25 12:55:06 +0000

    atualização part2

---
+++ aula2/incluirdisciplina.php
@@ -0,0 +1,69 @@
+<?php
+$msg = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $nome = $_POST["nome"];
+    $sigla = $_POST["sigla"];
+    $carga = $_POST["carga"];
+
+    echo "nome: $nome sigla: $sigla carga: $carga";
+
+    if(!file_exists("disciplinas.txt")){
+
+        $arqDisc = fopen("disciplinas.txt","w") or die("erro crítico na criação do arquivo");
+        $linha = "nome;sigla;carga\n";
+        fwrite($arqDisc, $linha);
+
+        fclose($arqDisc);
+    }
+    $arqDisc = fopen("disciplinas.txt","a") or die("erro critico na adição no arquivo");
+    $linha = $nome . ";" . $sigla . ";" .$carga.";";
+    fwrite($arqDisc, $linha);
+    fclose($arqDisc);
+    $msg = "Deu tudo certo";
+    
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inclusão de disciplina</title>
+    <style>
+        body {
+            font-family: sans-serif;
+            padding: 20px;
+        }
+        form {
+            display: flex;
+            flex-direction: column;
+            width: 300px;
+        }
+        input {
+            margin-bottom: 10px;
+            padding: 8px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+        input[type="submit"] {
+            background-color: #4CAF50;
+            color: white;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <form action="incluirdisciplina.php" method="POST" name="incluirdisciplina">
+        Nome da disciplina: <input type="text" name="nome"><br>
+        Sigla da disciplina: <input type="text" name="sigla"><br>
+        Carga horária da disciplina: <input type="number" name="carga">
+        <input type="submit" value="Enviar">
+    </form>
+    <?php
+    // Exibe a mensagem de sucesso se a variável $msg não estiver vazia
+    if (!empty($msg)) {
+        echo "<h1>$msg</h1>";
+    }
+    ?>
+</body>
+</html>
---
+++ aula2/listadisciplinas.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Lista Disciplina</h1>
+    <table>
+        <!--<tr><th>nome</th><th>sigla</th><th>carga</th></tr> -->
+    <?php
+    $arqDisc = fopen("disciplinas.txt", "r") or die("erro ao abrir");
+
+    while(!feof($arqDisc)){
+        $linha = fgets($arqDisc);
+        $colunaDados = explode(";", $linha);
+    
+    echo "<tr><td>".$colunaDados[0]."</td>".
+    "<td>".$colunaDados[1]."</td>".
+    "<td>".$colunaDados[2]."</td>". "</tr>";
+    }
+    fclose($arqDisc);
+    $msg = "deu certo";
+    ?>
+</body>
+</html>
